3a6490b4ad0fc30cc2f163eb5dd2a3bce3312956
ndominkiewicz <norbertd1410@gmail.com>
2025-12-21 18:34:59 +0100

    feat: assistance class for clean messages display

---
+++ audtream-desktop/src/main/java/com/norbdev/audtream/util/Logger.java
@@ -0,0 +1,21 @@
+package com.norbdev.audtream.util;
+
+public class Logger {
+
+    public static void printMsg(String message) {
+        System.out.println("[AUDTREAM]: " + message);
+    }
+
+    public static void printMsg(double value) {
+        System.out.println("[AUDTREAM]: " + value);
+    }
+
+    public static void printErr(String message) {
+        System.err.println("[AUDTREAM]: "  +message);
+    }
+
+    public static void printErr(double value) {
+        System.err.println("[AUDTREAM]: " + value);
+    }
+}
+
