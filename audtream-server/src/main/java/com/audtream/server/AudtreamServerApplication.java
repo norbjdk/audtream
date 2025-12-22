eac64166112592aed6f7e6a8963d1113bf537bc3
ndominkiewicz <norbertd1410@gmail.com>
2025-12-23 00:06:28 +0100

    Create spring-boot module for server service side.

---
+++ audtream-server/src/main/java/com/audtream/server/AudtreamServerApplication.java
@@ -0,0 +1,13 @@
+package com.audtream.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudtreamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudtreamServerApplication.class, args);
+	}
+
+}
---
+++ audtream-server/src/test/java/com/audtream/audtream_server/AudtreamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audtream.audtream_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudtreamServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
